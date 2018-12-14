afb5f7a377c6022c5c19cdbae703ed34519fe429
pointbaaar <pointbazaar@protonmail.com>
2018-12-14 14:23:03 +0100

    use the grid to give the services page a makeover

---
+++ src/main/resources/public/highlightcurrentpage.js
@@ -0,0 +1,10 @@
+//source:
+//https://stackoverflow.com/questions/26819675/navbar-highlight-for-current-page
+
+$(function(){
+    $('a').each(function(){
+        if ($(this).prop('href') == window.location.href) {
+            $(this).addClass('active'); $(this).parents('li').addClass('active');
+        }
+    });
+});
--- src/main/resources/public/loadheaderandfooter.js
+++ src/main/resources/public/loadheaderandfooter.js
@@ -3,4 +3,7 @@ $(function(){
   $("#header").load("logo_fragment.html");
   $("#footer").load("footer.html");
   $("#toolslogos").load("tools_logos_fragment.html");
+
+
+
 });
