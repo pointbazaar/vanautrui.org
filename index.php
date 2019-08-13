a830aecd7ab18f7629f21f88cba90ea3a54239f5
pointbazaar <alex23667@gmail.com>
2019-08-13 18:04:27 +0200

    update the website

--- index.php
+++ index.php
@@ -22,83 +22,10 @@
 </head>
 
 <body>
-  <?php
-    include("logo_fragment.php");
-  ?>
-
-  <div class="p-4"></div>
-  <div class="container">
-    <h2 class="m-3"> Web Development Company</h2>
-    <p class="english m-3"> We are a small enterprise. <br>
-        Our focus is to create reliable Web applications, websites and serve as a consulting team for customers.</p>
-
-    <br>
-    <div class=" ">
-        <div>
-            <div class="row ">
-                <img src="http://cc0-icons.vanautrui.org/composite/server.svg" style="width:12em;" class="m-3"/>
-                <div class="col justify-content-center ">
-                    <div>
-                        <h2 class="m-3">Websites</h2>
-                        <p class="m-3">
-                            We can design your Website using Bootstrap,Typescript and Angular, to represent your business.<br>
-                            All of the Websites we develop are secured using <a href="https://letsencrypt.org/"> Lets Encrypt Certificates</a> <br>
-                            Take a look at our Portfolio to see some Websites that <br>
-                            we have designed.
-                        </p>
-                        <p>Features include</p>
-                        <ul>
-                            <li>Mobile Responsive</li>
-                            <li>good page speed</li>
-                            <li>SSL Certificates with
-                                <a href="https://certbot.eff.org/">Certbot </a> for your Website
-                            </li>
-                            <li>Map with
-                                <a href="https://www.openstreetmap.org">Openstreetmap</a>
-                                and
-                                <a href="https://leafletjs.com/">Leaflet.js</a>
-			    </li>
-			    <li>
-				only using statically typed Languages, to prevent errors
-			   </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-        <hr>
-        <div>
-            <div class="row ">
-                <img src="http://cc0-icons.vanautrui.org/composite/servers.svg" style="width:12em;" class="m-3"/>
-                <div class="col justify-content-center">
-                    <div>
-                        <h2 class="m-3">Web Applications</h2>
-                        <p class="m-3">
-                            We offer custom Web Application development using Java(Spring Boot or our own Framework) and Haskell,<br>
-                            which goes from Consulting, Requirement Gathering and Design<br>
-                            to Development, Hosting and ongoing Maintenance.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-  </div>
-    <hr>
-  <div class="container">
-      <h3 class="text-center display-4">
-          Our Guarantee:
-      </h3>
-      <div class="text-center">
-          <p> You will always get the <strong>Source Code </strong>for the entire project.</p>
-          <p> We will develop 100% Open-Source Software </p>
-          <p> <strong>no Vendor Lock-In.</strong> <br>Because we only use opensource libraries and frameworks, <br>
-              everyone can build on our work.</p>
-      </div>
-  </div>
-
-  
-</body>
-
+    <?php
+        include("logo_fragment.php");
+    ?>
+    <?php
+        include("index_inner.html");
+    ?>
 </html>
