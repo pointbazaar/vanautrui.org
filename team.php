8009bb7e36c90b2d0def51785e31f3bbf9bea706
Alan I-Shref <alan.i-shref@mediacom.de>
2019-09-24 10:34:31 +0200

    Added Alan Profile, image and website will be added soon

--- team.php
+++ team.php
@@ -1,111 +1,136 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8" />
-  <title>Team - Van Autrui</title>
 
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<head>
+	<meta charset="utf-8" />
+	<title>Team - Van Autrui</title>
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 
 <body>
-  <?php
-    include("logo_fragment.php");
-  ?>
+	<?php
+	include("logo_fragment.php");
+	?>
 
-  <div class="p-4"></div>
+	<div class="p-4"></div>
 
-  <!-- https://stackoverflow.com/questions/43578264/how-to-make-image-in-bootstrap-thumbnail-square-and-responsive -->
-  <div class="container-fluid text-center">
-	  
-	  <h3 class="text-center">Team </h3>
-	  <div class="row justify-content-center">
+	<!-- https://stackoverflow.com/questions/43578264/how-to-make-image-in-bootstrap-thumbnail-square-and-responsive -->
+	<div class="container-fluid text-center">
 
-		  <div class="card m-3 ">
-			<div class="card-header bg-custom-dark">
-				<a href="" class="fg-custom">Aaron Frey</a>
+		<h3 class="text-center">Team </h3>
+		<div class="row justify-content-center">
+
+			<div class="card m-3 ">
+				<div class="card-header bg-custom-dark">
+					<a href="" class="fg-custom">Aaron Frey</a>
+				</div>
+				<img class="card-img-top profilepic" src="https://kreathon-krefeld.de/wp-content/uploads/2018/11/kreathon-2018-21.jpg">
+				<div class="card-footer bg-custom-dark">
+					<div>
+						<span class="badge badge-light">UI</span>
+						<span class="badge badge-light">UX</span>
+						<span class="badge badge-light">Frontend</span>
+					</div>
+					<div>
+						<span class="badge badge-light">Java</span>
+						<span class="badge badge-light">Adobe XD</span>
+					</div>
+				</div>
 			</div>
-			<img class="card-img-top profilepic" src="https://kreathon-krefeld.de/wp-content/uploads/2018/11/kreathon-2018-21.jpg">
+
+			<div class="card m-3 ">
+				<div class="card-header bg-custom-dark">
+					<a class="fg-custom" href="profiles/jorg.html">Alexander Hansen</a>
+				</div>
+
+				<img class="card-img-top profilepic" src="https://vanautrui.org/pictures/alex.jpg">
+
+				<div class="card-footer bg-custom-dark">
+					<div>
+						<span class="badge badge-light">Backend</span>
+						<span class="badge badge-light">Haskell</span>
+						<span class="badge badge-light">Bootstrap</span>
+					</div>
+					<div>
+						<span class="badge badge-light">Android</span>
+						<span class="badge badge-light">Java</span>
+					</div>
+				</div>
+			</div>
+
+			<div class="card m-3 ">
+				<div class="card-header bg-custom-dark">
+					<a class="fg-custom" href="profiles/david.html">David Milosevic</a>
+				</div>
+
+				<img class="card-img-top profilepic" src="https://vanautrui.org/pictures/david.jpeg">
+
+				<div class="card-footer bg-custom-dark">
+					<div>
+						<span class="badge badge-light">C</span>
+						<span class="badge badge-light">Linux Kernel Dev.</span>
+					</div>
+					<div>
+						<span class="badge badge-light">C++</span>
+						<span class="badge badge-light">Linux Driver Dev.</span>
+					</div>
+				</div>
+			</div>
+
+			<div class="card m-3 ">
+				<div class="card-header bg-custom-dark">
+					<a class="fg-custom" href="http://bestofcode.net/">Marco Steinke</a>
+				</div>
+
+				<img class="card-img-top profilepic" src="https://www.bestofcode.net/Dashboard/assets/img/pp.jpg">
+
+				<div class="card-footer bg-custom-dark">
+					<div>
+						<span class="badge badge-light">PHP</span>
+						<span class="badge badge-light">Web</span>
+						<span class="badge badge-light">Bootstrap</span>
+					</div>
+					<div>
+						<span class="badge badge-light">Java</span>
+						<span class="badge badge-light">Web Design</span>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="card m-3 ">
+			<div class="card-header bg-custom-dark">
+				<a class="fg-custom" href="#">Alan I-Shref</a>
+			</div>
+
+			<img class="card-img-top profilepic" src="">
+
 			<div class="card-footer bg-custom-dark">
 				<div>
-					<span class="badge badge-light">UI</span>
-					<span class="badge badge-light">UX</span>
-					<span class="badge badge-light">Frontend</span>
+					<span class="badge badge-light">Versatile</span>
+					<span class="badge badge-light">Fullstack Web</span>
+					<span class="badge badge-light">GameDev</span>
 				</div>
 				<div>
 					<span class="badge badge-light">Java</span>
-					<span class="badge badge-light">Adobe XD</span>
+					<span class="badge badge-light">C#</span>
+					<span class="badge badge-light">JavaScript</span>
+					<span class="badge badge-light">Python</span>
 				</div>
 			</div>
-		  </div>
+		</div>
+	</div>
 
-		  <div class="card m-3 ">
-			<div class="card-header bg-custom-dark">
-				<a class="fg-custom" href="profiles/jorg.html">Alexander Hansen</a>
-			</div>
+	<!-- inspiration: https://blog.hubspot.com/marketing/creative-agency-team-pages -->
+	<!-- https://www.rkh.co.uk/about-us -->
 
-			<img class="card-img-top profilepic" src="https://vanautrui.org/pictures/alex.jpg">
 
-			<div class="card-footer bg-custom-dark">
-				<div>
-					<span class="badge badge-light">Backend</span>
-					<span class="badge badge-light">Haskell</span>
-					<span class="badge badge-light">Bootstrap</span>
-				</div>
-				<div>
-					<span class="badge badge-light">Android</span>
-					<span class="badge badge-light">Java</span>
-				</div>
-			</div>
-		  </div>
-		  <div class="card m-3 ">
-			<div class="card-header bg-custom-dark">
-				<a class="fg-custom" href="profiles/david.html">David Milosevic</a>
-			</div>
-
-			<img class="card-img-top profilepic" src="https://vanautrui.org/pictures/david.jpeg">
-
-			<div class="card-footer bg-custom-dark">
-				<div>
-					<span class="badge badge-light">C</span>
-					<span class="badge badge-light">Linux Kernel Dev.</span>
-				</div>
-				<div>
-					<span class="badge badge-light">C++</span>
-					<span class="badge badge-light">Linux Driver Dev.</span>
-				</div>
-			</div>
-		  </div>
-		  <div class="card m-3 ">
-			<div class="card-header bg-custom-dark">
-				<a class="fg-custom" href="http://bestofcode.net/">Marco Steinke</a>
-			</div>
-
-			<img class="card-img-top profilepic" src="https://www.bestofcode.net/Dashboard/assets/img/pp.jpg">
-
-			<div class="card-footer bg-custom-dark">
-				<div>
-					<span class="badge badge-light">PHP</span>
-					<span class="badge badge-light">Web</span>
-					<span class="badge badge-light">Bootstrap</span>
-				</div>
-				<div>
-					<span class="badge badge-light">Java</span>
-					<span class="badge badge-light">Web Design</span>
-				</div>
-			</div>
-		  </div>
-	  </div>
- 
-  </div>
-
-  <!-- inspiration: https://blog.hubspot.com/marketing/creative-agency-team-pages -->
-  <!-- https://www.rkh.co.uk/about-us -->
-
-  
 
 </body>
+
 </html>
